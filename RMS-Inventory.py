e4b8e5295c0c12c7598185136377f3536372d910
EthanHans3n <1ethanhansen@gmail.com>
2018-07-23 21:12:42 -0700

    Initial upload

---
+++ RMS-Inventory.py
@@ -0,0 +1,6 @@
+#load text socument with inventory into the program here
+entered = str(input("Options: add, change, delete ")).lower()
+while (entered != "exit") :
+    if (entered == "add") :
+        #get more responses to add to the dictionary
+    entered = str(input("Options: add, change, delete ")).lower()
