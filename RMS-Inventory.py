f8dfbc5e4e0595234e4a3da57196308e0ad490cb
Ethan Hansen <1ethanhansen@gmail.com>
2018-07-24 19:03:56 -0700

    lots added, now there will be info for fixtures as well as for items
    fixtures() and items() functions made

--- RMS-Inventory.py
+++ RMS-Inventory.py
@@ -3,17 +3,48 @@ file = open('DO_NOT_TOUCH_INVENTORY.txt', 'r')
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #put code for reading text document into dictionary here
+inventory = {
+}
 
-#take in input and make it a lower case string
-entered = str(input("Options: find, add, change, delete, print ")).lower()
-#while the user hasn't entered "exit", keep the program running
-while (entered != "exit") :
-    #if the user entered "add"
-    if (entered == "add") :
-        #get more responses to add to the dictionary
-        name = str(input("Enter the name of the item ")).lower()
-        location = str(input("Enter the location of the item (room.shelf.row.column): ")).lower()
-        
-    entered = str(input("Options: add, change, delete ")).lower()
+def fixtures(entered):
+    entered = str(input("Options: find, add")).lower()
 
-#just testing github
+def items(entered) :
+    #while the user hasn't entered "exit", keep the program running
+    while (entered != "exit" and entered != "e") :
+        #if the user entered "add"
+        if (entered == "add" or entered == "a") :
+            #get more responses to add to the dictionary
+            name = str(input("Enter the name of the item to add ")).lower()
+            location = str(input("Enter the location of the item (room.zone.row.description): ")).lower()
+            #add the new item
+            inventory[name] = location
+        if (entered == "print" or entered == "p") :
+            print(inventory)
+        if (entered == "delete" or entered == "d") :
+            #get the name of the item and make it lowercase
+            name = str(input("Enter the name of the item to delete ")).lower()
+            #delete from the dictionary inventory the item with that name
+            del(inventory[name])
+        if (entered == "change" or entered == "c") :
+            name = str(input("Enter the name of the item to change ")).lower()
+            if name in inventory:
+                location = str(input("Enter the new location of " + name + ": ")).lower()
+                inventory[name] = location
+            else:
+                print("Couldn't find that, try something else, or add an item")
+        if (entered == "open the pod bay doors, hal") :
+            print("I'm sorry Dave, I'm afraid I can't do that")
+        else:
+            name = entered
+            if name in inventory:
+                print("The location of " + name + " is " + inventory[name])
+            else :
+                print("That doesn't exist, try another name for the item or add an item")
+        entered = str(input("Options: find, add, change, delete, print, exit ")).lower()
+
+entered = str(input("Options: add, change, delete, print, exit ")).lower()
+if (entered[:1] == "!") :
+    fixtures(entered[1:])
+else :
+    items(entered)
