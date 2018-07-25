8d236beda3ff3286c4f91fa1d0c479e529d88a40
Ethan Hansen <1ethanhansen@gmail.com>
2018-07-24 19:22:36 -0700

    added instructions, debugging

--- RMS-Inventory.py
+++ RMS-Inventory.py
@@ -7,7 +7,7 @@ inventory = {
 }
 
 def fixtures(entered):
-    entered = str(input("Options: find, add")).lower()
+    entered = str(input("Options: add")).lower()
 
 def items(entered) :
     #while the user hasn't entered "exit", keep the program running
@@ -19,31 +19,32 @@ def items(entered) :
             location = str(input("Enter the location of the item (room.zone.row.description): ")).lower()
             #add the new item
             inventory[name] = location
-        if (entered == "print" or entered == "p") :
+        elif (entered == "print" or entered == "p") :
             print(inventory)
-        if (entered == "delete" or entered == "d") :
+        elif (entered == "delete" or entered == "d") :
             #get the name of the item and make it lowercase
             name = str(input("Enter the name of the item to delete ")).lower()
             #delete from the dictionary inventory the item with that name
             del(inventory[name])
-        if (entered == "change" or entered == "c") :
+        elif (entered == "change" or entered == "c") :
             name = str(input("Enter the name of the item to change ")).lower()
             if name in inventory:
                 location = str(input("Enter the new location of " + name + ": ")).lower()
                 inventory[name] = location
             else:
                 print("Couldn't find that, try something else, or add an item")
-        if (entered == "open the pod bay doors, hal") :
+        elif (entered == "open the pod bay doors, hal") :
             print("I'm sorry Dave, I'm afraid I can't do that")
         else:
+            print("else running\n\n" + entered)
             name = entered
             if name in inventory:
                 print("The location of " + name + " is " + inventory[name])
             else :
                 print("That doesn't exist, try another name for the item or add an item")
-        entered = str(input("Options: find, add, change, delete, print, exit ")).lower()
+        entered = str(input("Options:  add, change, delete, print, exit ")).lower()
 
-entered = str(input("Options: add, change, delete, print, exit ")).lower()
+entered = str(input("Options: add, change, delete, print, exit, or just enter the name of the item to find (for fixtures enter ! then the number ex. !9999) ")).lower()
 if (entered[:1] == "!") :
     fixtures(entered[1:])
 else :
